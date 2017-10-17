5dbe53fdd07c2c44851d4a4479b81aaf5c55e53f
Yuliyan <y.georgiev@student.tue.nl>
2017-10-17 19:21:37 +0200

    Added owner giving food
    For some reason the food is falling through the terrain

---
+++ GameDesignProject/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    public GameObject food;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Object"))
+        {
+            Destroy(other.gameObject);
+            Instantiate( food, transform.position, transform.rotation);
+            // Play good boy sound here
+        }
+    }
+}
