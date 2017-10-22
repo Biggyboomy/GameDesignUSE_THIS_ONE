7b43a65c7e003dc2a93d12a55c3eb748e7c61b55
JeroenHageman <jeroen7991@live.nl>
2017-10-22 21:38:00 +0200

    objectives

---
+++ GameDesignProject/Assets/Scripts/New Folder/Objective.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpawnPoop
+{
+    public class Objective : MonoBehaviour
+    {
+        public GameObject ObjectiveSign_01;
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.name == "Objective_01" && (Input.GetKeyDown("r")))
+            {
+                ObjectiveSign_01.SetActive(false);
+            }
+        }
+    }
+}
