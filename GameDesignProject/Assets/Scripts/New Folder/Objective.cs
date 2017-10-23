bcc26511f84a0f8f72aa09733ec847497e2ce580
JeroenHageman <jeroen7991@live.nl>
2017-10-23 12:41:24 +0200

    Objective_fixed

--- GameDesignProject/Assets/Scripts/New Folder/Objective.cs
+++ GameDesignProject/Assets/Scripts/New Folder/Objective.cs
@@ -6,10 +6,16 @@ namespace SpawnPoop
     public class Objective : MonoBehaviour
     {
         public GameObject ObjectiveSign_01;
+        public GameObject ThirdPersonController;
+        private SpawnPoop poopCountScript;
+        private int PoopCount;
 
         void OnTriggerStay(Collider other)
         {
-            if (other.gameObject.name == "Objective_01" && (Input.GetKeyDown("r")))
+            poopCountScript = ThirdPersonController.GetComponent<SpawnPoop>();
+            PoopCount = poopCountScript.poopCount;
+
+            if (other.gameObject.name == "Objective_01" && (Input.GetKeyDown("r")) && PoopCount != 0)
             {
                 ObjectiveSign_01.SetActive(false);
             }
