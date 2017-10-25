03150ff614168c617ae76b79bf4c26257f79d77f
JeroenHageman <jeroen7991@live.nl>
2017-10-25 18:25:45 +0200

    Objectives

--- GameDesignProject/Assets/Scripts/New Folder/Objective.cs
+++ GameDesignProject/Assets/Scripts/New Folder/Objective.cs
@@ -6,6 +6,10 @@ namespace SpawnPoop
     public class Objective : MonoBehaviour
     {
         public GameObject ObjectiveSign_01;
+        public GameObject ObjectiveSign_02;
+        public GameObject ObjectiveSign_03;
+        public GameObject ObjectiveSign_04;
+        public GameObject ObjectiveSign_05;
         public GameObject ThirdPersonController;
         private SpawnPoop poopCountScript;
         private int PoopCount;
@@ -18,6 +22,31 @@ namespace SpawnPoop
             if (other.gameObject.name == "Objective_01" && (Input.GetKeyDown("r")) && PoopCount != 0)
             {
                 ObjectiveSign_01.SetActive(false);
+                ScoreScript.scoreValue += 1;
+            }
+
+            if (other.gameObject.name == "Objective_02" && (Input.GetKeyDown("r")) && PoopCount != 0)
+            {
+                ObjectiveSign_02.SetActive(false);
+                ScoreScript.scoreValue += 1;
+            }
+
+            if (other.gameObject.name == "Objective_03" && (Input.GetKeyDown("r")) && PoopCount != 0)
+            {
+                ObjectiveSign_03.SetActive(false);
+                ScoreScript.scoreValue += 1;
+            }
+
+            if (other.gameObject.name == "Objective_04" && (Input.GetKeyDown("r")) && PoopCount != 0)
+            {
+                ObjectiveSign_04.SetActive(false);
+                ScoreScript.scoreValue += 1;
+            }
+
+            if (other.gameObject.name == "Objective_05" && (Input.GetKeyDown("r")) && PoopCount != 0)
+            {
+                ObjectiveSign_05.SetActive(false);
+                ScoreScript.scoreValue += 1;
             }
         }
     }
